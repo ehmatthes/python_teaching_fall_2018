9b5dbc210d7dd0cb74ad80d9bedefe0f875a47ca
Eric Matthes <ehmatthes@gmail.com>
2018-12-10 11:10:32 -0900

    Alien ship appears, and moves across the screen.

---
+++ sideways_hero_nb_example/alien_ship.py
@@ -0,0 +1,30 @@
+import pygame
+
+from random import randint
+
+class AlienShip:
+    """A class to manage the ship."""
+
+    def __init__(self, sh_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = sh_game.screen
+        self.screen_rect = sh_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/alien_ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at a random position on the right
+        #  side of the screen
+        self.rect.left = self.screen_rect.right
+        self.rect.y = randint(0, self.screen_rect.height)
+
+    def update(self):
+        # The alien ships will always move from right to left,
+        #  and randomly up and down.
+        self.rect.x -= 1
+        self.rect.y += randint(-5, 5)
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
--- sideways_hero_nb_example/sh_game.py
+++ sideways_hero_nb_example/sh_game.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from ship import Ship
+from alien_ship import AlienShip
 
 class SidewaysHero:
     """Overall class to manage game assets and behavior."""
@@ -15,51 +16,7 @@ class SidewaysHero:
         pygame.display.set_caption("Sideways Hero")
 
         self.ship = Ship(self)
-
-    def run_game(self):
-        """Start the main loop for the game."""
-        while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True                           # Look here
-                elif event.type == pygame.KEYUP:                                #   and here
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-
-            # Update positions of any characters that might be moving.
-            self.ship.update()                                                  # and here
-
-            self.screen.fill((255, 255, 255))
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-
-if __name__ == '__main__':
-    # Make a game instance, and run the game.
-    sh_game = SidewaysHero()
-    sh_game.run_game()
-import sys
-
-import pygame
-
-from ship import Ship
-
-class SidewaysHero:
-    """Overall class to manage game assets and behavior."""
-
-    def __init__(self):
-        """Initialize the game, and create game resources."""
-        pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Sideways Hero")
-
-        self.ship = Ship(self)
+        self.alien_ship = AlienShip(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -77,11 +34,11 @@ class SidewaysHero:
 
             # Update positions of any characters that might be moving.
             self.ship.update()
+            self.alien_ship.update()
 
-            # Redraw the background, and then draw all characters using their
-            #  new positions.
             self.screen.fill((255, 255, 255))
             self.ship.blitme()
+            self.alien_ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
